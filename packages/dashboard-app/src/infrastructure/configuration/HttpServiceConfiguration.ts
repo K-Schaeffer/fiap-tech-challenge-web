4e2640b7b0da363327f310032c7483ee58a06f10
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-05-13 20:51:49 -0300

    refactor: move sesrviceProvider to actual configuration service

---
+++ packages/dashboard-app/src/infrastructure/configuration/HttpServiceConfiguration.ts
@@ -0,0 +1,29 @@
+import { AccountService } from "@/application/services/AccountService";
+import { TransactionService } from "@/application/services/TransactionService";
+import { HttpAccountRepository } from "../repositories/HttpAccountRepository";
+import { HttpTransactionRepository } from "../repositories/HttpTransactionRepository";
+
+export interface HttpServices {
+  accountService: AccountService;
+  transactionService: TransactionService;
+}
+
+let httpServices: HttpServices | null = null;
+
+export function initializeHttpServices(): HttpServices {
+  if (!httpServices) {
+    const repositories = {
+      accountRepository: new HttpAccountRepository(),
+      transactionRepository: new HttpTransactionRepository(),
+    };
+
+    httpServices = {
+      accountService: new AccountService(repositories.accountRepository),
+      transactionService: new TransactionService(
+        repositories.transactionRepository,
+        repositories.accountRepository
+      ),
+    };
+  }
+  return httpServices;
+}
