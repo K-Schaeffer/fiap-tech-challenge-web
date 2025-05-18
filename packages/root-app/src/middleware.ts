f70c717b4b3eda9b145cb4e3c9db2c0baf81e2c6
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-05-18 20:39:37 -0300

    docs: update readme and remove unnecessary log

--- packages/root-app/src/middleware.ts
+++ packages/root-app/src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("auth_token");
-  console.log("token ", token);
   const isProtectedPage = request.nextUrl.pathname.startsWith("/dashboard");
 
   // Protect dashboard route
