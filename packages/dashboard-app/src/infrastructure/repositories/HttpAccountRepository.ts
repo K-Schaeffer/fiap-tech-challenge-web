5947da3b8eca6840af3bd705810a8b5af72d2f6f
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-05-18 18:34:17 -0300

    feat: add HttpClient to reuse http methods with security on dashboard

--- packages/dashboard-app/src/infrastructure/repositories/HttpAccountRepository.ts
+++ packages/dashboard-app/src/infrastructure/repositories/HttpAccountRepository.ts
@@ -1,18 +1,17 @@
 import { Account, AccountAttributes } from "@/domain/entities/Account";
 import { AccountRepository } from "@/domain/repositories/AccountRepository";
+import { HttpClient } from "../utils/HttpClient";
 
 export class HttpAccountRepository implements AccountRepository {
-  private readonly baseUrl = "http://localhost:5000";
+  private readonly client: HttpClient;
+
+  constructor() {
+    this.client = new HttpClient("http://localhost:5000");
+  }
 
   async getAccountInfo(): Promise<Account> {
-    const res = await fetch(`${this.baseUrl}/account`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const data = await this.client.get<AccountAttributes>("/account");
 
-    const data: AccountAttributes = await res.json();
     return new Account(data);
   }
 }
