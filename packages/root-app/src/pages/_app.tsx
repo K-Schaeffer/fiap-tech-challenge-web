f1250e2590f74458df315eaa5f0113da2604339c
K-Schaeffer <kcb.schaeffer@gmail.com>
2025-05-18 21:52:13 -0300

    feat: start tracking core web vitals

--- packages/root-app/src/pages/_app.tsx
+++ packages/root-app/src/pages/_app.tsx
@@ -3,9 +3,14 @@ import ThemeProviderWrapper from "@/theme/ThemeProviderWrapper";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import { AppProps } from "next/app";
+import { useReportWebVitals } from "next/web-vitals";
 import "./globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useReportWebVitals((metric) => {
+    console.log(metric.name, metric.value, metric.rating);
+  });
+
   return (
     <AppRouterCacheProvider options={{ key: "css" }}>
       <ThemeProviderWrapper>
